7df6628a5b04d84fa4c6da4935edb3e892d4ff75
tnrbns <tsrubinos@up.edu.ph>
2024-04-29 11:28:03 +0800

    functions working

--- gui.py
+++ gui.py
@@ -1,5 +1,41 @@
 from primality import primalCheck
-from factorial import factorial
+from factorial import fact
+import tkinter as tk
+
+def isPrime(new_val):
+    if primalCheck(new_val)==True:
+        value1["text"] = str(new_val) + " True dawg"
+    else: value1["text"] = str(new_val) + " Naw not"
+
+def factorial(new_val):
+    value2["text"] = fact(new_val)
+
+def handle_click():
+    value = int(entry.get())
+    isPrime(value)
+    factorial(value)
+    
+# Declare new 
+window = tk.Tk()
+
+greeting = tk.Label(text="Input entry")
+
+entry = tk.Entry()
+submit = tk.Button(text="Submit",command=handle_click)
+frame = tk.Frame(width=400, height=100)
+value1 = tk.Label(text="prime")
+value2 = tk.Label(text="fact")
 
 
-print("test")
+greeting.pack()
+entry.pack()
+submit.pack()
+frame.pack()
+value1.pack()
+value2.pack()
+
+
+
+
+# Main event must be executed
+window.mainloop()
--- primality.py
+++ primality.py
@@ -1,8 +1,8 @@
 from math import sqrt # Module needed for sqrt() function
-# Function to check if number is prime
 
+# Function to check if number is prime
 def primalCheck(n):
-    if n < 2:
+    if n < 2: 
         return False
     for i in range(2, int(sqrt(n)) + 1):
         if n % i == 0:
